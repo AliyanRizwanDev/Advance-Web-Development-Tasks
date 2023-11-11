e9f99f7684a0e4288ee0b3ee13e2775faabc6045
Aliyan Rizwan <102222781+AliyanRizwanDev@users.noreply.github.com>
2023-11-12 02:22:27 +0500

    Add files via upload

---
+++ Practice/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Student> StdList;
+        public HomeController()
+        {
+            StdList = new List<Student>();
+        }
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Student std)
+        {
+            StdList.Add(std);
+            return View();
+        }
+
+
+        public ActionResult AdminDashBoard()
+        {
+            return View();
+        }
+	}
+}
