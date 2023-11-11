796cdaea6be0569ab04601bbec0e847c4672ff46
Aliyan Rizwan <102222781+AliyanRizwanDev@users.noreply.github.com>
2023-11-12 02:15:53 +0500

    Add files via upload

---
+++ Form Submission/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Student> mystds;
+        public HomeController()
+        {
+            mystds = new List<Student>();
+            mystds.Add(new Student { Id = 1, Name = "asad" });
+            mystds.Add(new Student { Id = 2, Name = "Kamal" });
+            mystds.Add(new Student { Id = 3, Name = "Ali" });
+        }
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View(mystds);
+        }
+        [HttpPost]
+        public ActionResult Index(Student std)
+        {
+            mystds.Add(std);
+            return View(mystds);
+        }
+	}
+}
